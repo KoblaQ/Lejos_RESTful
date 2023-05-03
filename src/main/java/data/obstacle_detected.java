9268e34780aae49c05c95fdd24d4c33d32abfa3b
Edem <edemq123@gmail.com>
2023-05-03 10:19:31 +0300

    obstacle detected class created

--- src/main/java/data/obstacle_detected.java
+++ src/main/java/data/obstacle_detected.java
@@ -11,6 +11,24 @@ public class obstacle_detected {
 	private int id;
 	private int distance;
 	private String time;
+	
+	
+	public obstacle_detected() {
+		super();
+	}
+	public obstacle_detected (int id, String time) {
+		super();
+		this.id = id;
+		this.time = time;
+	}
+	
+	public obstacle_detected (int id, int distance, String time) {
+		super();
+		this.id = id;
+		this.distance = distance;
+		this.time = time;
+	}
+	
 	public int getId() {
 		return id;
 	}
